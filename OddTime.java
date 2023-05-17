59c5176093c62fe663b41db228cd6cc6e1e4327b
hoangdoan22 <130956043+hoangdoan22@users.noreply.github.com>
2023-05-17 18:15:01 +0700

    Add files via upload

--- OddTime.java
+++ OddTime.java
@@ -1,15 +1,17 @@
 package HomeworkDSday2;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class OddTime {
 	public static void main(String[] args) {
-		int[] array = { 1, 1, 2, 2, 3, 1 };
+		int[] array = { 1, 1, 2, 2, 3, 1, 5, 2 };
 		System.out.println(findOddTimesAppearNum(array));
 	}
 
-	private static int findOddTimesAppearNum(int[] array) {
+	private static ArrayList<Integer> findOddTimesAppearNum(int[] array) {
 		HashMap<Integer, Integer> hashmap = new HashMap<>();
+		ArrayList<Integer> oddTimes = new ArrayList<>();
 		for (int i = 0; i < array.length; i++) {
 			if (hashmap.containsKey(array[i])) {
 				int value = hashmap.get(array[i]);
@@ -17,14 +19,14 @@ public class OddTime {
 			} else
 				hashmap.put(array[i], 1);
 		}
-		int count = 0;
+
 		for (int key : hashmap.keySet()) {
 
 			if (hashmap.get(key) % 2 != 0) {
-				count++;
+				oddTimes.add(key);
 			}
 		}
-		return count;
+		return oddTimes;
 
 	}
 }
